996399cf23876cb53fc044029ef07e9b60348217
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-26 22:05:29 -0400

    Update NFT1.sol

--- NFT1.sol
+++ NFT1.sol
@@ -7,8 +7,26 @@ import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract DungeonsAndDragonsCharacter is ERC721, VRFConsumerBase, Ownable {
+contract NFT1 is ERC721, VRFConsumerBase, Ownable {
     using SafeMath for uint256;
     using Strings for string;
     
+     bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+    address public VRFCoordinator;
+    // rinkeby: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+    address public LinkToken;
+    // rinkeby: 0x01BE23585060835E02B77ef475b0Cc51aA1e0709a
+
+    struct Character {
+        uint256 strength;
+        uint256 dexterity;
+        uint256 constitution;
+        uint256 intelligence;
+        uint256 wisdom;
+        uint256 charisma;
+        uint256 experience;
+        string name;
+    }
    }
